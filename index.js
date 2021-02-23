aec0a213c43c3026a89265704921530f2a3e7e15
daria-kawecka <dusiek2806@gmail.com>
2021-02-23 20:33:27 +0100

    add bot status

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
-require("dotenv").config();
-const Discord = require("discord.js");
-const client = new Discord.Client();
+fs = require("fs");
 
+const Discord = require("discord.js");
+
+const { token, prefix } = require("./config.json");
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+//Bot power on:
 client.on("ready", () => {
-  console.log(`Cześć, cześć! Tutaj ja, ${client.user.tag}!`);
+  console.log("Ready to do some stuff!!!");
+  client.user.setPresence({
+    activity: {
+      name: "studiuję, bo lubię",
+      type: 0,
+    },
+  });
 });
 
-client.on("message", (msg) => {
-  if (msg.content.includes("piwo")) {
-    msg.reply("Jedno, dwa lub więcej!!!");
+//messages
+//commands collection:
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (command) {
+    try {
+      client.commands.get(command).run(message, args, client);
+    } catch (error) {
+      message.channel.send(
+        "Ziomuś, nie mam pojęcia o co Ci chodzi... skup się albo użyj *!info* 🤔"
+      );
+    }
   }
 });
 
 //greeting new member on server:
 client.on("guildMemberAdd", (member) => {
   const channel = member.guild.channels.cache.find(
-    (ch) => ch.name === "member-log"
+    (ch) => ch.name === "ogólny" || ch.name === "ynlógo"
   );
   if (!channel) return;
   channel.send(
@@ -23,4 +55,4 @@ client.on("guildMemberAdd", (member) => {
   );
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
